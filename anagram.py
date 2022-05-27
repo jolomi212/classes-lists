2640ef4a88ab6c3b1166f6095a9e6f7f001486d0
jolomi212 <65557108+jolomi212@users.noreply.github.com>
2022-05-27 21:14:50 +0100

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,25 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+word= input("Enter the first word here \n")
+word2= input("Enter the second word here \n")
+
+word = word.lower()
+word2 = word2.lower()
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+   
+    word_check = sorted(word)
+    word2_check = sorted(word2)
+
+    if len(word.split())==len(word2.split()) and word_check==word2_check:
+        print("The given words are anagrams")
+        return True
+    else:
+        print("The given words are not anagrams")
+
+
+find_anagram(word, word2)
---
+++ readfile/main.py
@@ -0,0 +1,34 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    new_file = open(filename, "r")
+    read_file = new_file.read()
+
+    new_file.close() 
+    return read_file
+
+
+
+def count_words(str):
+    text = read_file_content(str)
+    # [assignment] Add your code here
+    
+    def counter(str):
+        wordcount = dict()
+        words = str.split()
+
+        for word in words:
+            if word in wordcount:
+                wordcount[word] += 1
+
+            else:
+                wordcount[word] = 1
+
+        return (wordcount)       
+    return(counter(text))
+
+print(count_words("story.txt"))
